65b4580917a387c21adb3f48f63032903d3fb5f7
N.O.Atomusk <orzkey@outlook.com>
2022-06-17 00:51:07 +0800

    tutorial01 完成！

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,13 +1,24 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+typedef enum
+{
+    LEPT_NULL,
+    LEPT_FALSE,
+    LEPT_TRUE,
+    LEPT_NUMBER,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+} lept_type;
 
-typedef struct {
+typedef struct
+{
     lept_type type;
-}lept_value;
+} lept_value;
 
-enum {
+enum
+{
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
